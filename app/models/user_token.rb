24528de20bb143798a4694cb5c8c488186ca7a28
DylanCa <dylan.cattelan@gmail.com>
2025-02-10 17:20:12 +0100

    Created table UserToken + migration file

---
+++ app/models/user_token.rb
@@ -0,0 +1,14 @@
+class UserToken < ApplicationRecord
+  belongs_to :user
+
+  validates :access_token, presence: true
+  validates :refresh_token, presence: true, uniqueness: true
+  validates :expires_at, presence: true
+
+  scope :active, -> { where('expires_at > ?', Time.current) }
+  scope :expired, -> { where('expires_at <= ?', Time.current) }
+
+  def expired?
+    expires_at <= Time.current
+  end
+end
