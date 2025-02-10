050dc6cb1eaa1dfd3ec54ce21f3eb21c0f3c6b82
DylanCa <dylan.cattelan@gmail.com>
2025-02-10 17:21:47 +0100

    Updated User model associations & added github_client method to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+  has_one :github_account
+  has_one :user_stat
+
+  has_many :projects
+  has_many :user_tokens
+
+  accepts_nested_attributes_for :github_account
+  accepts_nested_attributes_for :user_tokens
+  accepts_nested_attributes_for :user_stat
+
+  enum :account_status, { active: 1, disabled: 0, banned: -1 }
+
+  scope :with_github_id, ->(github_id) {
+    joins(:github_account).where(github_account: { github_id: github_id })
+  }
+
+  def github_client
+    client = Octokit::Client.new(access_token: github_account.oauth_access_token)
+    return client if client.user_authenticated?
+
+    # TODO: Implement refresh token logic here
+    raise Octokit::Error
+  end
 end
