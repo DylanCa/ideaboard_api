c59dc1f37bbbfcd7f4bfeaf2c1d1b5746802a54c
DylanCa <dylan.cattelan@gmail.com>
2025-02-12 17:52:52 +0100

    Updated Github models to better fit incoming Github data

--- app/models/github/issue.rb
+++ app/models/github/issue.rb
@@ -1,33 +1,37 @@
 module Github
   class Issue < T::Struct
-    const :id, Integer
-    const :number, Integer
+    const :full_database_id, Integer
     const :title, String
+    const :url, String
+    const :number, Integer
     const :state, String
     const :repository_id, Integer
-    const :repository_name, String
-    const :author_username, String
+    const :author_username, T.nilable(String)
     const :comments_count, Integer
-    const :reactions, Integer
-    const :created_at, Time
-    const :updated_at, Time
-    const :closed_at, T.nilable(Time) # can be nil
+    const :reactions_count, Integer
+    const :created_at, String
+    const :updated_at, String
+    const :closed_at, T.nilable(String) # can be nil
 
-    def self.from_github(data)
+    def self.from_github(data, repo_id = nil)
       new(
-        id: data.id,
-        number: data.number,
+        full_database_id: data.full_database_id.to_i,
         title: data.title,
+        url: data.url,
+        number: data.number,
         state: data.state,
-        repository_id: data.repository.id,
-        repository_name: data.repository.full_name,
-        author_username: data.user.login,
-        comments_count: data.comments,
-        reactions: parse_reactions(data.reactions),
+        repository_id: repo_id || data.repository.database_id,
+        author_username: data.author&.login,
+        comments_count: data.comments.total_count,
+        reactions_count: data.reactions.total_count,
         created_at: data.created_at,
         updated_at: data.updated_at,
         closed_at: data.closed_at
       )
+    end
+
+    def self.from_array(data, repo_id = nil)
+      data.map { |i| from_github(i, repo_id) }
     end
 
     def open?
--- app/models/github/repository.rb
+++ app/models/github/repository.rb
@@ -1,36 +1,40 @@
 module Github
   class Repository < T::Struct
-    const :id, Integer
+    const :github_id, Integer
     const :name, String
-    const :full_name, String
+    const :name_with_owner, String
+    const :pull_requests, T.nilable(T::Array[PullRequest])
+    const :issues, T.nilable(T::Array[Issue])
     const :description, T.nilable(String)
-    const :language, T.nilable(String)
-    const :fork, T::Boolean
+    const :primary_language, T.nilable(String)
+    const :is_fork, T::Boolean
     const :stars_count, Integer
     const :forks_count, Integer
-    const :open_issues_count, Integer
+    const :total_commits_count, Integer
     const :archived, T::Boolean
     const :disabled, T::Boolean
     const :license, T.nilable(String)
-    const :created_at, Time
-    const :updated_at, Time
+    const :github_created_at, String
+    const :github_updated_at, String
 
     def self.from_github(data)
       new(
-        id: data.id,
+        github_id: data.database_id,
         name: data.name,
-        full_name: data.full_name,
+        name_with_owner: data.name_with_owner,
+        pull_requests: Helpers.extract_pull_requests(data),
+        issues: Helpers.extract_issues(data),
         description: data.description,
-        language: data.language,
-        fork: data.fork,
-        stars_count: data.stargazers_count,
-        forks_count: data.forks_count,
-        open_issues_count: data.open_issues_count,
-        archived: data.archived,
-        disabled: data.disabled,
-        license: data.license&.key,
-        created_at: data.created_at,
-        updated_at: data.updated_at
+        primary_language: data.primary_language&.name,
+        is_fork: data.is_fork,
+        stars_count: data.stargazer_count,
+        forks_count: data.fork_count,
+        archived: data.is_archived,
+        disabled: data.is_disabled,
+        license: data.license_info&.key,
+        github_created_at: data.created_at,
+        github_updated_at: data.updated_at,
+        total_commits_count: data.default_branch_ref&.target&.history&.total_count || 0,
       )
     end
 
