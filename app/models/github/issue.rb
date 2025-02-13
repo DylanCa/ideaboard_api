8709e969b1da2a83fd23539c0d35f1a84d7b2c41
DylanCa <dylan.cattelan@gmail.com>
2025-02-13 15:49:28 +0100

    Updated Issue and PR Github models to better fit incoming data + db

--- app/models/github/issue.rb
+++ app/models/github/issue.rb
@@ -1,6 +1,6 @@
 module Github
   class Issue < T::Struct
-    const :full_database_id, Integer
+    const :github_id, String
     const :title, String
     const :url, String
     const :number, Integer
@@ -8,14 +8,13 @@ module Github
     const :github_repository_id, Integer
     const :author_username, T.nilable(String)
     const :comments_count, Integer
-    const :reactions_count, Integer
-    const :created_at, String
-    const :updated_at, String
+    const :github_created_at, String
+    const :github_updated_at, String
     const :closed_at, T.nilable(String) # can be nil
 
     def self.from_github(data, repo_id = nil)
       new(
-        full_database_id: data.full_database_id.to_i,
+        github_id: data.id,
         title: data.title,
         url: data.url,
         number: data.number,
@@ -23,15 +22,30 @@ module Github
         github_repository_id: repo_id,
         author_username: data.author&.login,
         comments_count: data.comments.total_count,
-        reactions_count: data.reactions.total_count,
-        created_at: data.created_at,
-        updated_at: data.updated_at,
+        github_created_at: data.created_at,
+        github_updated_at: data.updated_at,
         closed_at: data.closed_at
       )
     end
 
     def self.from_array(data, repo_id = nil)
       data.map { |i| from_github(i, repo_id) }
+    end
+
+    def stringify_keys
+      {
+        github_id: github_id,
+        title: title,
+        url: url,
+        number: number,
+        state: state,
+        github_repository_id: github_repository_id,
+        author_username: author_username,
+        comments_count: comments_count,
+        github_created_at: github_created_at,
+        github_updated_at: github_updated_at,
+        closed_at: closed_at
+      }
     end
 
     def open?
--- app/models/github/pull_request.rb
+++ app/models/github/pull_request.rb
@@ -1,6 +1,6 @@
 module Github
   class PullRequest < T::Struct
-    const :full_database_id, Integer
+    const :github_id, String
     const :title, String
     const :url, String
     const :number, Integer
@@ -9,20 +9,15 @@ module Github
     const :author_username, T.nilable(String)
     const :merged_at, T.nilable(String)
     const :closed_at, T.nilable(String)
-    const :created_at, String
-    const :updated_at, String
+    const :github_created_at, String
+    const :github_updated_at, String
     const :is_draft, T::Boolean
-    const :mergeable, T.nilable(String)
-    const :can_be_rebased, T.nilable(T::Boolean)
     const :total_comments_count, Integer
     const :commits, Integer
-    const :additions, Integer
-    const :deletions, Integer
-    const :changed_files, Integer
 
     def self.from_github(data, repo_id = nil)
       new(
-        full_database_id: data.full_database_id.to_i,
+        github_id: data.id,
         title: data.title,
         url: data.url,
         number: data.number,
@@ -31,21 +26,35 @@ module Github
         author_username: data.author&.login,
         merged_at: data.merged_at,
         closed_at: data.closed_at,
-        created_at: data.created_at,
-        updated_at: data.updated_at,
+        github_created_at: data.created_at,
+        github_updated_at: data.updated_at,
         is_draft: data.is_draft,
-        mergeable: data.mergeable,
-        can_be_rebased: data.can_be_rebased,
         total_comments_count: data.total_comments_count,
         commits: data.commits.total_count,
-        additions: data.additions,
-        deletions: data.deletions,
-        changed_files: data.changed_files,
       )
     end
 
     def self.from_array(data, repo_id = nil)
       data.map { |pr| from_github(pr, repo_id) }
+    end
+
+    def stringify_keys
+      {
+        github_id: github_id,
+        title: title,
+        url: url,
+        number: number,
+        state: state,
+        github_repository_id: github_repository_id,
+        author_username: author_username,
+        merged_at: merged_at,
+        closed_at: closed_at,
+        github_created_at: github_created_at,
+        github_updated_at: github_updated_at,
+        is_draft: is_draft,
+        total_comments_count: total_comments_count,
+        commits: commits
+      }
     end
 
     def open?
