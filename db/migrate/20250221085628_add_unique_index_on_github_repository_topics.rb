4c0efbbaec149e0eb86aff24387091611ad53f11
DylanCa <dylan.cattelan@gmail.com>
2025-02-21 12:16:17 +0100

    Renamed Tags to Topics

---
+++ db/migrate/20250221085628_add_unique_index_on_github_repository_topics.rb
@@ -0,0 +1,11 @@
+class AddUniqueIndexOnGithubRepositoryTopics < ActiveRecord::Migration[8.0]
+  def up
+    rename_column :github_repository_topics, :tag_id, :topic_id
+    add_index :github_repository_topics, [:github_repository_id, :topic_id], unique: true
+  end
+
+  def down
+    remove_index :github_repository_topics, column: [:github_repository_id, :topic_id]
+    rename_column :github_repository_topics, :topic_id, :tag_id
+  end
+end
