3c9da344364a14b7f550b5f4a59c5028c93aef7c
DylanCa <dylan.cattelan@gmail.com>
2025-02-26 14:34:31 +0100

    Modified RepositoryDataFetcherWorker logic to be based on repo full name

--- app/sidekiq/repository_data_fetcher_worker.rb
+++ app/sidekiq/repository_data_fetcher_worker.rb
@@ -3,15 +3,10 @@ class RepositoryDataFetcherWorker
 
   sidekiq_options queue: :default, retry: 3
 
-  def perform(repo_id)
-    repo = GithubRepository.find_by(id: repo_id)
-    return if repo.nil?
+  def perform(repo_full_name)
+    repo_id = RepositoryFetcherWorker.new.perform(repo_full_name)["id"]
 
-    # Step 1: Update repository metadata
-    RepositoryFetcherWorker.perform_async(repo.full_name)
-
-    # Step 2: Schedule items fetching with a delay to avoid rate limiting
-    ItemsFetcherWorker.perform_async(repo.id, "prs")
-    ItemsFetcherWorker.perform_async(repo.id, "issues")
+    ItemsFetcherWorker.perform_async(repo_id, "prs")
+    ItemsFetcherWorker.perform_async(repo_id, "issues")
   end
 end
