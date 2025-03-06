c9613301214d1ed510f284bf06ac64ec15eecc8b
DylanCa <dylan.cattelan@gmail.com>
2025-03-06 17:55:44 +0100

    Improved GraphQL mocking in specs

--- spec/support/graphql_mocks.rb
+++ spec/support/graphql_mocks.rb
@@ -28,7 +28,47 @@ module GraphQLMocks
                          query: mock_response_obj
     )
 
+    allow(mock_client).to receive(:parse).and_return(double('GraphQL::Client::OperationDefinition'))
     allow(Github).to receive(:client).and_return(mock_client)
+    mock_response_obj
+  end
+
+  def mock_github_query_with_variables(query_name, variables, fixture_file = nil)
+    fixture_file ||= query_name.to_s.underscore
+    fixture_path = Rails.root.join("spec/fixtures/github_api/#{fixture_file}.json")
+
+    raise "Fixture not found: #{fixture_path}" unless File.exist?(fixture_path)
+
+    deep_ostruct_from_hash = lambda do |obj|
+      case obj
+      when Hash
+        OpenStruct.new(obj.transform_values(&deep_ostruct_from_hash))
+      when Array
+        obj.map(&deep_ostruct_from_hash)
+      else
+        obj
+      end
+    end
+
+    raw_mock_response = JSON.parse(File.read(fixture_path))
+    mock_response = deep_transform_keys(raw_mock_response)
+
+    mock_response_obj = OpenStruct.new(
+      data: deep_ostruct_from_hash.call(mock_response),
+      errors: nil
+    )
+
+    # Create a more complete double that handles both query and parse methods
+    mock_client = double('GraphQL::Client')
+    allow(mock_client).to receive(:query).and_return(mock_response_obj)
+    allow(mock_client).to receive(:query).with(
+      any_args,
+      hash_including(variables: variables)
+    ).and_return(mock_response_obj)
+
+    allow(mock_client).to receive(:parse).and_return(double('GraphQL::Client::OperationDefinition'))
+    allow(Github).to receive(:client).and_return(mock_client)
+    mock_response_obj
   end
 
   def deep_transform_keys(obj)
