a181d20377c7777ec95dee1a34c84f709041635b
DylanCa <dylan.cattelan@gmail.com>
2025-03-06 23:53:54 +0100

    Added gem shoulda-matchers to specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,13 @@ RSpec.configure do |config|
 end
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+
+require 'shoulda/matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
